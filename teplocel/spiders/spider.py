200a9d47c66ded6f6662b80287df4c54659d1ccc
Vitaly Svyatyuk <goodgame1945@gmail.com>
2017-08-01 10:31:00 +0300

    with auth cookies

--- teplocel/spiders/spider.py
+++ teplocel/spiders/spider.py
@@ -1,19 +1,42 @@
 # -*- coding: utf-8 -*-
-from scrapy import Spider, Request
+from scrapy import Spider, Request, FormRequest
 import csv
 
 
 class ItemSpider(Spider):
     name = "teplocel"
-    start_urls = ["http://www.teplocel.ru/catalog/",]
+    start_urls = ["http://www.teplocel.ru/catalog"]
 
+
+
+    # def parse(self, response):
+    #     return FormRequest.from_response(
+    #         response,
+    #         formdata={'username': '2632803528', 'password': '112265'},
+    #         callback=self.after_login
+    #     )
+
+    # def after_login(self, response):
     def parse(self, response):
+        # if "authentication failed" in response.body:
+        #     self.logger.error("Login failed")
+        #     return
         with open('items.csv', 'w') as csv_file:
             writer = csv.writer(csv_file, delimiter=';')
             writer.writerow(['Title', 'Price', 'URL'])
         links = response.xpath('//div[@class="catalog-main-categories"]/div/ul/li/a/@href').extract()
         for link in links:
-            yield Request(response.urljoin(link), callback=self.get_pages)
+            # yield Request(response.urljoin(link),
+            yield Request(response.urljoin(link), cookies={'_ym_uid': '1500296968748492942',
+                                                           'BITRIX_SM_SOUND_LOGIN_PLAYED': 'Y',
+                                                           '_ym_isad': '2',
+                                                           'PHPSESSID': 'b37udeo0ru06kao7qjl93gbgs3',
+                                                           '_ga': 'GA1.2.2028308846.1500296968',
+                                                           '_gid': 'GA1.2.1841182279.1501513499',
+                                                           '_gat': '1', '_ym_visorc_33143708':'w',
+                                                           'BITRIX_SM_SALE_UID': '1193853',
+                                                           'BITRIX_SM_LOGIN': '2632803528'},
+                          callback=self.get_pages)
 
     def get_pages(self, response):
         pages = response.xpath('//div[@class="pager"]/ul/li[last()]/a/@data-page').extract_first()
